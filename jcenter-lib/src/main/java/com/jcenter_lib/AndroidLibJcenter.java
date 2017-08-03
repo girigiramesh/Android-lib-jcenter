e675a3c088b996299b1ea538c47ba07665d484e7
Ramesh Girigi <ramesh@kvana.com>
2017-08-03 12:20:06 +0530

    first commit

---
+++ jcenter-lib/src/main/java/com/jcenter_lib/AndroidLibJcenter.java
@@ -0,0 +1,11 @@
+package com.jcenter_lib;
+
+/**
+ * Created by Ramesh on 8/3/17.
+ */
+
+public class AndroidLibJcenter {
+    public static String helloAndroid() {
+        return "Hello Android..!";
+    }
+}
